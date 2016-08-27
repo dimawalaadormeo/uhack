71fc9b881af23bfacd0a69c3dfe6842fbd049c11
Moshe Adormeo <madormeo@localhost.localdomain>
2016-08-27 19:27:41 +0800

    First commit

---
+++ gen/com/teamapril/uhack/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.teamapril.uhack;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/teamapril/uhack/MainActivity.java
@@ -0,0 +1,230 @@
+package com.teamapril.uhack;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.hardware.Camera.Face;
+import android.hardware.Camera.FaceDetectionListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.OrientationEventListener;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageButton;
+
+public class MainActivity extends ActionBarActivity implements SurfaceHolder.Callback {
+	public static final String TAG = MainActivity.class.getSimpleName();
+
+    private Camera mCamera;
+
+    // We need the phone orientation to correctly draw the overlay:
+    private int mOrientation;
+    private int mOrientationCompensation;
+    private OrientationEventListener mOrientationEventListener;
+
+    // Let's keep track of the display rotation and orientation also:
+    private int mDisplayRotation;
+    private int mDisplayOrientation;
+    ImageButton captureButton;
+
+    // Holds the Face Detection result:
+    private Camera.Face[] mFaces;
+
+    // The surface view for the camera data
+    private SurfaceView mView;
+
+    // Draw rectangles and other fancy stuff:
+    private FaceOverlayView mFaceView;
+
+    /**
+     * Sets the faces for the overlay view, so it can be updated
+     * and the face overlays will be drawn again.
+     */
+    private FaceDetectionListener faceDetectionListener = new FaceDetectionListener() {
+        @Override
+        public void onFaceDetection(Face[] faces, Camera camera) {
+            Log.d("onFaceDetection", "Number of Faces:" + faces.length);
+            // Update the view now!
+            mFaceView.setFaces(faces);
+        }
+    };
+
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mView = (SurfaceView) findViewById(R.id.surfaceView);
+	     /*   mView = new SurfaceView(this);
+
+        setContentView(mView, new LayoutParams(600, 600));*/
+        // Now create the OverlayView:
+        mFaceView = new FaceOverlayView(this);
+        addContentView(mFaceView, new LayoutParams(mView.getLayoutParams()));
+        // Create and Start the OrientationListener:
+        mOrientationEventListener = new SimpleOrientationEventListener(this);
+        mOrientationEventListener.enable();
+
+        captureButton = (ImageButton) findViewById(R.id.btnCapture);
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                mCamera.takePicture(null, null, mPicture);
+                    }
+                });
+            }
+        });
+	}
+	
+	Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap bitmapPicture = BitmapFactory.decodeByteArray(data,0,data.length);
+                 //   Log.d("IMAGE",encodeToBase64(bitmapPicture, Bitmap.CompressFormat.PNG, 100));
+                    captureButton.setImageBitmap(bitmapPicture);
+                    mCamera.startPreview();
+        }
+    };
+    
+    public String encodeToBase64(Bitmap image, Bitmap.CompressFormat compressFormat, int quality)
+    {
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        image.compress(compressFormat, quality, byteArrayOS);
+        return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        SurfaceHolder holder = mView.getHolder();
+        holder.addCallback(this);
+    }
+
+    @Override
+    protected void onPause() {
+        mOrientationEventListener.disable();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        mOrientationEventListener.enable();
+        super.onResume();
+    }
+    
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        mCamera = Camera.open();
+        mCamera.setFaceDetectionListener(faceDetectionListener);
+        mCamera.startFaceDetection();
+        try {
+            mCamera.setPreviewDisplay(surfaceHolder);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not preview the image.", e);
+        }
+    }
+
+
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+        // We have no surface, return immediately:
+        if (surfaceHolder.getSurface() == null) {
+            return;
+        }
+        // Try to stop the current preview:
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            // Ignore...
+        }
+        // Get the supported preview sizes:
+        Camera.Parameters parameters = mCamera.getParameters();
+        List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
+        Camera.Size previewSize = previewSizes.get(0);
+        // And set them:
+        parameters.setPreviewSize(previewSize.width, previewSize.height);
+        mCamera.setParameters(parameters);
+        // Now set the display orientation for the camera. Can we do this differently?
+        mDisplayRotation = Util.getDisplayRotation(MainActivity.this);
+        mDisplayOrientation = Util.getDisplayOrientation(mDisplayRotation, 0);
+        mCamera.setDisplayOrientation(mDisplayOrientation);
+
+        if (mFaceView != null) {
+            mFaceView.setDisplayOrientation(mDisplayOrientation);
+        }
+
+        // Finally start the camera preview again:
+        mCamera.startPreview();
+    }
+
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        mCamera.setPreviewCallback(null);
+        mCamera.setFaceDetectionListener(null);
+        mCamera.setErrorCallback(null);
+        mCamera.release();
+        mCamera = null;
+    }
+
+    /**
+     * We need to react on OrientationEvents to rotate the screen and
+     * update the views.
+     */
+    private class SimpleOrientationEventListener extends OrientationEventListener {
+
+        public SimpleOrientationEventListener(Context context) {
+            super(context, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+            // We keep the last known orientation. So if the user first orient
+            // the camera then point the camera to floor or sky, we still have
+            // the correct orientation.
+            if (orientation == ORIENTATION_UNKNOWN) return;
+            mOrientation = Util.roundOrientation(orientation, mOrientation);
+            // When the screen is unlocked, display rotation may change. Always
+            // calculate the up-to-date orientationCompensation.
+            int orientationCompensation = mOrientation
+                    + Util.getDisplayRotation(MainActivity.this);
+            if (mOrientationCompensation != orientationCompensation) {
+                mOrientationCompensation = orientationCompensation;
+                mFaceView.setOrientation(mOrientationCompensation);
+            }
+        }
+    }
+    
+    
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
